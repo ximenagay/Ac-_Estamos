41a352ca7ceb018614c8322233777cca292479a1
franciscovega <fran.vgasalv@gmail.com>
2024-02-09 09:33:57 -0300

    commitFinal

--- FRONTEND/javascript/enrolamientoTalent.js
+++ FRONTEND/javascript/enrolamientoTalent.js
@@ -1,14 +1,11 @@
 export function selectAvatar(element) {
-  // Deseleccionar avatares previamente seleccionados
   var avatars = document.querySelectorAll(".avatar-option");
   avatars.forEach(function (avatar) {
     avatar.classList.remove("selected-avatar");
   });
 
-  // Seleccionar el avatar actual
   element.classList.add("selected-avatar");
 
-  // Actualizar la imagen de vista previa
   var avatarPreview = document
     .getElementById("avatar-preview")
     .querySelector("img");
